4fd89ca267ab2bdf4f3c59edb88b4decdb249b60
RUDRASOOD <87219127+RUDRASOOD@users.noreply.github.com>
2022-01-11 20:08:12 +1100

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,13 +1,31 @@
 img = "";
+status1 = "";
 
 function setup() {
     canvas = createCanvas(640, 420)
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById
 }
 function preload() {
-    img = loadImage('Dag.jpg');
+    img = loadImage('dag.jpg');
 }
 
 function draw() {
     image(img, 0, 0, 640, 420);
+
 }
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status1 = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
+
